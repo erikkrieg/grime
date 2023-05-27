e4ef04cd6249f274a382b1abcf6d02140ad79eb6
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-05-27 17:20:31 -0400

    was having issues with rust lsp support that was fixed by using my older nix shell config

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  rust_overlay = import (builtins.fetchTarball https://github.com/oxalica/rust-overlay/archive/master.tar.gz);
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+    extensions = [ "rust-src" ];
+  });
+in
+with pkgs;
+mkShell {
+  nativeBuildInputs = [
+    buildPackages.just
+    rust
+    rust-analyzer
+  ];
+}
