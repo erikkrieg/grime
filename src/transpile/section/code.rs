f4b539e427d76cd6765265f3b74e0540ca44999f
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-06-01 23:01:41 -0400

    adds unit tests for code section

--- src/transpile/section/code.rs
+++ src/transpile/section/code.rs
@@ -14,17 +14,70 @@ pub fn replace(
     if line == "```" {
         rules.block.paragraph = false;
         html_out = "<code>\n".into();
-        // TODO: Check and error if code block doesn't close.
         html_out += &lines
             .take_while(|l| l.trim() != "```")
-            .fold(Html::new(), |h, l| {
-                println!("line: {l}");
-                format!("{h}{}\n", l.trim_end())
-            });
-        // Currently this means that hitting the EOF will also close the code.
-        // If this happens unintentionally it should be a pretty clear so going
-        // to let this slide for now.
+            .fold(Html::new(), |h, l| format!("{h}{}\n", l.trim_end()));
         html_out += "</code>";
     }
     html_out
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    fn setup(markdown: &str) -> (&str, ParserRules, Peekable<impl Iterator<Item = &str>>) {
+        let rules = ParserRules::default();
+        let mut lines = markdown.lines().peekable();
+        let line = lines.next().unwrap();
+        (line, rules, lines)
+    }
+
+    #[test]
+    fn code_section_replace() {
+        let markdown = "```
+    #[test]
+    fn code_section_replace() { ... }
+```";
+        let html = "<code>
+    #[test]
+    fn code_section_replace() { ... }
+</code>";
+        let (line, mut rules, mut lines) = setup(markdown);
+        let parsed = replace(line, &mut rules, &mut lines);
+        assert_eq!(parsed, html);
+    }
+
+    #[test]
+    fn ingores_lines_outside_code_section() {
+        let markdown = "```
+    #[test]
+    fn code_section_replace() { ... }
+```
+below";
+        let html = "<code>
+    #[test]
+    fn code_section_replace() { ... }
+</code>";
+        let (line, mut rules, mut lines) = setup(markdown);
+        let parsed = replace(line, &mut rules, &mut lines);
+        assert_eq!(parsed, html);
+    }
+
+    #[test]
+    // This is an implicit feature the results from the current implementation.
+    // Changing this behavior might be a breaking change.
+    fn code_section_no_closing_backticks() {
+        let markdown = "```
+    #[test]
+    fn code_section_replace() { ... }";
+        let html = "<code>
+    #[test]
+    fn code_section_replace() { ... }
+</code>";
+        let (line, mut rules, mut lines) = setup(markdown);
+        let parsed = replace(line, &mut rules, &mut lines);
+        assert_eq!(parsed, html);
+    }
 }
