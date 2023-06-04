7dcaa3fb205be6b5c5e365174e1a69d04a703957
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2023-06-04 16:44:27 -0400

    fix nix linter warning

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 let
-  rust_overlay = import (builtins.fetchTarball https://github.com/oxalica/rust-overlay/archive/master.tar.gz);
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
   pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
   rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
     extensions = [ "rust-src" ];
